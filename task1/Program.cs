a369068d64bd619f00fc612a0d95f57d8a31de9b
maksotik <maksotik@rambler.ru>
2022-03-19 00:09:06 +0300

    Задача 1- является ли число палиндромом, решение с использованием строки

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// С помощью строки, с отрицательными числами не работает
+
+Console.WriteLine("Введите число-> ");
+string number = Console.ReadLine();
+
+if(number.Length == 5){
+    
+    if(number[0] == number[4] && number[1] == number[3]){
+        Console.WriteLine("Число {0} является палиндромом.", number);
+    }else Console.WriteLine("Число {0} не является палиндромом", number);
+
+}else Console.WriteLine("Число не пятизначное!");
