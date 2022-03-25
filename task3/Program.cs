0cd63cab8c55aabcdbda37f42f5333b3ee501484
maksotik <maksotik@rambler.ru>
2022-03-26 02:05:40 +0300

    Задача 3 по нахождению кубов до введенного числа, включая его

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число - ");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+int cube;
+
+Console.Write("Результат - ");
+while( i <= number){
+    cube = i * i * i;
+    i++;
+    Console.Write("{0}   ", cube);
+}
+Console.WriteLine();
