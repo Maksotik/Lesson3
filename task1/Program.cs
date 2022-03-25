4bf2241ef51d0ff23958ab88768947b25c636acb
maksotik <maksotik@rambler.ru>
2022-03-26 00:34:00 +0300

    Является ли число палиндромом с использованием массива и метода

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,14 +1,37 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
-// является ли оно палиндромом.
-// С помощью строки, с отрицательными числами не работает
+﻿// Напишите программу, которая принимает на вход ЛЮБОЕ число типа int и проверяет,
+// является ли оно палиндромом, с помощью массива.
 
-Console.WriteLine("Введите число-> ");
-string number = Console.ReadLine();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int [] array = new int [digit_counter(number)]; // Создаем массив, размер задает функция
+int i = array.Length; // Переменная используется для счетчика последней ячейки, в следующем цикле.
+int max_index = array.Length - 1; // Переменная для указания индекса последней ячейки цикла for
 
-if(number.Length == 5){
-    
-    if(number[0] == number[4] && number[1] == number[3]){
-        Console.WriteLine("Число {0} является палиндромом.", number);
-    }else Console.WriteLine("Число {0} не является палиндромом", number);
+// Цикл в котором мы заполняем массив цифрами введенного числа
+while( i != 0 ){   
+    i--;  
+    array [i] = number % 10;
+    number /= 10; 
+}
 
-}else Console.WriteLine("Число не пятизначное!");
+// Цикл в котором мы сравниваем ячейки массива(первую с последней, всторую с предпоследней и.т.д.)
+for ( int count = 0; count <= max_index; count++ ){
+
+if( array[count] == array[max_index] )
+    max_index--;
+else{
+    Console.WriteLine("Введенное число НЕ является палиндромом! "); 
+    return;   
+} 
+
+}
+Console.WriteLine("Введенное число палиндром!");
+
+// Метод подсчета количества цифр в числе.
+int digit_counter(int N){   
+int i = 0;
+while( N != 0 ){
+    N /= 10;
+    i++;
+}return i;
+}
